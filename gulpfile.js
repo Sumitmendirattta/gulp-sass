d004e440789be3455d42dfb3d08374aa54931936
Sumitmendirattta <sumit.mendiratta@sjsu.edu>
2018-04-15 15:58:26 -0700

    Implement gulp watch and browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
+'use strict';
+
 var gulp = require('gulp');
 
 // load gulp-sass plugin
 var sass = require('gulp-sass');
+
+var browserSync = require('browser-sync').create();
 
 // define the gulp "task" that will compile your sass
 /* We make a few assumptions here.  Change as your project requires:
@@ -9,11 +13,24 @@ var sass = require('gulp-sass');
 *  2. Your sass files using the "Sassy CSS" syntax, and thus end in .scss
 *  3. Your CSS stylesheets will live in the stylesheets/ directory at the root of your project
 */
+// gulp.task('sass', function() {
+//   gulp.src('scss/*.scss')
+//     .pipe(sass())
+//     .pipe(gulp.dest('stylesheets'));
+// });
+
 gulp.task('sass', function() {
-  gulp.src('scss/*.scss')
+  return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss
     .pipe(sass())
-    .pipe(gulp.dest('stylesheets'));
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
+
+gulp.task('watch', ['array', 'of', 'tasks', 'to', 'complete','before', 'watch'], function (){
+  // ...
+})
 
 // define the gulp task that will watch for changes in your sass
 gulp.task('watch', function() {
@@ -33,3 +50,19 @@ gulp.task('gulp-compile-scss', function () {
     .pipe(sass()) // Sends it through a gulp plugin
     .pipe(gulp.dest('app/css')) // Outputs the file in the destination folder
 })
+
+gulp.task('watch', function() {
+  gulp.watch('app/scss/**/*.scss', ['sass']); 
+});
+
+
+gulp.task('sass-error', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
