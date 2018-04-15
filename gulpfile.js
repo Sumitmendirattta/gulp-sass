a99e843dadbed248a3eefb608b5b8e7da3604d63
Sumitmendirattta <sumit.mendiratta@sjsu.edu>
2018-04-15 15:25:56 -0700

    Setting Up node gulp sass

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+
+// load gulp-sass plugin
+var sass = require('gulp-sass');
+
+// define the gulp "task" that will compile your sass
+/* We make a few assumptions here.  Change as your project requires:
+*  1. Your sass files are in a scss/ directory at the root of your project
+*  2. Your sass files using the "Sassy CSS" syntax, and thus end in .scss
+*  3. Your CSS stylesheets will live in the stylesheets/ directory at the root of your project
+*/
+gulp.task('sass', function() {
+  gulp.src('scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('stylesheets'));
+});
+
+// define the gulp task that will watch for changes in your sass
+gulp.task('watch', function() {
+  gulp.watch('scss/*.scss', ['sass']);
+});
+
+// define the default gulp task
+gulp.task('default', ['sass', 'watch']);
+
+gulp.task('hello', function() {
+    console.log('Hello Zell');
+});
